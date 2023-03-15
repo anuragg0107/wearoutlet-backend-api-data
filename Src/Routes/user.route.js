de4a95b6dd50ecf7c9fea5048f09855042695bc9
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2023-03-15 19:38:34 +0530

    push backend data wearoutletc

---
+++ Src/Routes/user.route.js
@@ -0,0 +1,80 @@
+const express=require('express')
+const jwt=require('jsonwebtoken')
+const bcrypt=require('bcrypt')
+require("dotenv").config()
+// hi
+const {userModel}=require("../models/userModel")
+const userRouter=express.Router();
+
+userRouter.get("/",async(req,res)=>{
+    const data=await userModel.find()
+    res.send(data)
+})
+
+userRouter.post("/register",async(req,res)=>{
+    const {firstname,lastname,mobile,email,password}=req.body
+    const userPresent=await userModel.findOne({email:email})
+    if(userPresent?.email){
+        res.send({msg:"User is already exists please login"})
+    }
+    else{
+        try{
+        bcrypt.hash(password,6,async(err,secure_password)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                const user=new userModel({
+                    firstname,
+                    lastname,
+                    mobile,
+                    email,
+                    password:secure_password,
+                })
+                await user.save()
+                res.send({msg:"Register Successful go to login"})
+            }
+        })
+        }
+        catch(err){
+          res.send({msg:"Error during register the user"})
+          console.log(`${e}`)
+        }
+    }
+})
+
+
+userRouter.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+    try{
+        const user=await userModel.find({email})
+        console.log(user[0])
+        if(user.length>0){
+            bcrypt.compare(password,user[0].password,(err,result)=>{
+                if(result){
+                    const token=jwt.sign(
+                        {userID:user[0]._id},
+                        process.env.secretKey
+                    )
+                    res.send({
+                        msg:"Login Successful",
+                        token:token,
+                        user_name:user[0].name,
+                    })
+                }
+                else{
+                    res.send({msg:"Wrong Credentials"})
+                }
+            })
+        }
+        else{
+            res.send({msg:"Wrong Credentials"})
+        }
+    }
+    catch(err){
+        res.send({msg:"Login Failed"})
+        console.log(`${err}`);
+    }
+})
+
+module.exports={userRouter}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const app=express()
+const cors=require('cors')
+const {userRouter}=require("./Src/Routes/user.route")
+const {connection}=require("./Src/db/db")
+
+// hi
+app.use(
+    cors({
+        origin:"*"
+    })
+)
+app.use(express.json())
+
+app.use("/users",userRouter)
+
+app.get("/",(req,res)=>{
+    res.send("Home Page")
+})
+
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("Successful connected to db ");
+    }
+    catch(err){
+        console.log({msg:`${err}`});
+    }
+})
